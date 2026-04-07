98e29617a690ffef9eacbea18dff64e68c601be1
Raul Sofia <rauljcsofia@gmail.com>
2026-04-07 04:48:36 +0100

    spawn mode

--- pharmacophore2mol/models/bonder/train.py
+++ pharmacophore2mol/models/bonder/train.py
@@ -157,6 +157,7 @@ def main(config):
             pin_memory=True,
             prefetch_factor=4,
             persistent_workers=True,
+            multiprocessing_context="spawn",
         )
         val_loader = DataLoader(
             val_dataset,
@@ -167,6 +168,7 @@ def main(config):
             pin_memory=True,
             prefetch_factor=4,
             persistent_workers=True,
+            multiprocessing_context="spawn",
         )
 
         model = DenseBondPredictor(
